045ff52c45458221d401069ab3917a5d4ea5807b
Joiyad <20bee012@nith.ac.in>
2022-08-27 17:38:39 +0530

    changes

--- src/components/Content.js
+++ src/components/Content.js
@@ -161,10 +161,10 @@ const Content = ({ hideSidebar }) => {
             )
           )}
           <div className="hidden md:flex w-full">
-            <div className="h-20 bg-gradient-to-r from-blue-200 to-white w-1/4"><div className="bg-blue-300 h-16 mt-2"></div></div>
-            <div className="h-16 bg-gradient-to-r from-blue-200 to-white w-1/4"><div className="bg-blue-400 h-12 mt-2"></div></div>
-            <div className="h-8 bg-blue-600 w-1/4"><div className="bg-blue-500 h-8 mt-2"></div></div>
-            <div className="h-6 bg-blue-700 w-1/4"></div>
+            <div className="h-16 bg-gradient-to-r from-blue-200 to-white w-1/4"><div className="bg-gradient-to-r from-blue-100 to-blue-200 h-12 mt-2"></div></div>
+            <div className="h-12 mt-2 bg-gradient-to-r from-blue-200 to-white w-1/4"><div className="bg-gradient-to-r from-blue-200 to-blue-300 h-8 mt-2"></div></div>
+            <div className="h-10 pt-4 bg-blue-600 w-1/4"><div className="bg-gradient-to-r from-blue-300 to-blue-400 h-6 mt-2"></div></div>
+            <div className="h-6 bg-blue-700 w-1/4 mt-6"></div>
           </div>
         </div>
 
